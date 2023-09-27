101cca13f155f42dbbf6be8294f088cf3c3235d9
SUNDEEP SHRESTHA <sundeepshrestha22@augustana.edu>
2023-09-27 16:48:11 -0500

    "Now with dancing chicken"

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,11 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello bird giworld!");
+
+        System.out.println("Hello bird world!");
+
+
+        Chicken chicken = new Chicken();
+        chicken.dance();
     }
 }
